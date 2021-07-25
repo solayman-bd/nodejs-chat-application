3935c56d3db1ea60d42d5d4a6d668bf423142ff8
solayman-bd <mdsolaymannuhin@gmail.com>
2021-07-25 23:49:34 +0600

    defalt and not found error handler added successfully

---
+++ app.js
@@ -0,0 +1,47 @@
+//external import
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+//internal import
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middleware/common/errorHandler");
+
+const app = express();
+dotenv.config();
+
+//database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error));
+
+//request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing setup
+
+//404 not found handler
+app.use(notFoundHandler);
+//common error handler
+app.use(errorHandler);
+app.listen(process.env.PORT, () => {
+  console.log(`App listing to PORT ${process.env.PORT}`);
+});
